3f5012f499a6c551f0249a827401d7652a0a2e22
Britt Dalton <31601138+brittdalton@users.noreply.github.com>
2020-01-27 22:24:19 -0700

    add link to scripts

---
+++ Week_01/Scripts/script.js
@@ -0,0 +1,11 @@
+Console.log("Hello World!")
+
+console.log("Fav_website_Netflix")
+console.log("Fav_website_Pottery_barn_online_catalog")
+console.log("Fav_website_Creative_live")
+console.log("Fav_game_Sims4")
+console.log("Fav_game_Mario64")
+console.log("Fav_game_CaesarIV")
+console.log("Fav_artist_Laura_Berger")
+console.log("Fav_artist_Lindsay_Adler")
+console.log("Fav_artist_Von_Wong")
